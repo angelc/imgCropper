e9532f26a1c4f367b053f8c966a60de247e0c236
Alejandro Lechuga <ing.ramon.alejandro@gmail.com>
2011-12-11 00:53:07 -0800

    created created created

---
+++ imgCropper.js
@@ -0,0 +1,35 @@
+var imageCrop=function(params){
+    var _pub={};
+    _pub.limit={};
+    _pub.limit.minWidth=60;
+    _pub.limit.minHeight=60;
+    _pub.imgProperties={};
+    _pub.imgProperties.Width=0;
+    _pub.imgProperties.Height=0;
+    _pub.container;
+    _pub.img;
+   
+    _pub.init=function(){
+        _pub.container=document.getElementById(params.container);
+        _pub.img = _pub.getImage();   
+        console.log(_pub.img);
+        console.log(_pub.container);
+    };
+    _pub.getImage = function() {
+        var imgArray = _pub.container.getElementsByTagName(“img”),
+        img = false;
+        if (imgArray.length > 0)
+             img = imgArray[0];
+               
+        return img;
+
+};
+    _pub.getSize=function(img){
+        var width=img.width;
+        var height=img.height;   
+    };
+
+    //git>sitema de versionamiento
+    //github>redsocial de repositorio opensource
+    return _pub;
+};
